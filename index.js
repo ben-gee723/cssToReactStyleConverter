41019a29afe829bda22c5081e739f9d07e9d25f9
ben-gee723 <ben.m.goldschmidt@gmail.com>
2022-02-25 14:04:01 +0100

    convert multiple CSS to React style function added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const arrayToObject = (array) => {
 }
 
 const convertOneCssToReact = (string) => {
-    if (typeof string !== "String") return "Not a string!"
+    if (typeof string !== "string") return "Not a string!"
 
     // remove - unecessary spaces
     let noSpaces = string.replace(/\s/g, '');
@@ -24,31 +24,47 @@ const convertOneCssToReact = (string) => {
     for (let i = 0; i < noSpaces.length; i++) {
         noSpaces[i] === "{" ? insertedDivisions = [(noSpaces.slice(0, i) + " " + noSpaces.slice(i))].join('') : "";
     }
+    // console.log(insertedDivisions.split(" "))
 
     // deconstruct - main key and main object
-    let [newKey, newValue] = insertedDivisions.split(" ");
+    let [firstHalf, secondHalf] = insertedDivisions.split(" ");
+
+    // edit - key
+    let newKey = ""
 
     // create - an array
-    let secondHalf = newValue.slice(1).slice(0, -2).split(";").join(":").split(":");
+    let newValue = secondHalf.slice(1).slice(0, -2).split(";").join(":").split(":");
 
     // convert - array to object
-    let newObject = arrayToObject(secondHalf);
+    let newObject = arrayToObject(newValue);
 
     // set - new array in to React CSS object format!
     let reactCss = {};
-    reactCss[newKey] = newObject;
+    reactCss[firstHalf] = newObject;
     return reactCss;
+
+    // 2nd Version
+    // return [newKey, newObject];
 }
 
 // console.log(convertOneCssToReact(testCss1))
 // console.log(convertOneCssToReact(1))
 
 
-let testCss2 = `.bg-main {width: 100%; background-image: url("./bg-bluehex.jpg");background-position: top;background-repeat: no-repeat;background-size: cover;},.bg-main {width: 100%; background-image: url("./bg-bluehex.jpg");background-position: top;background-repeat: no-repeat;background-size: cover;},`;
+let testCss2 = `.bg-main {width: 100%; background-image: url("./bg-bluehex.jpg");background-position: top;background-repeat: no-repeat;background-size: cover;},.apples {width: 100%; background-image: url("./bg-bluehex.jpg");background-position: top;background-repeat: no-repeat;background-size: cover;}`;
 
 
 // To think odd and even
 const cssToReactStyleMultiple = (string) => {
+    let newObject = {};
+    string.split(",").map(x => {
+        // loop through all CSSs
+        // combine all objects together
+        newObject[Object.keys(convertOneCssToReact(x))[0]] = Object.values(convertOneCssToReact(x))[0];
 
+        // 2nd Version
+        // return newObject[convertOneCssToReact(x)[0]] = convertOneCssToReact(x)[1]
+    })
+    return newObject;
 }
 console.log(cssToReactStyleMultiple(testCss2))
