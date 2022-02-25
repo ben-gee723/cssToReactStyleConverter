b141a79653e59e13f69162b25a66f6b828e9212d
ben-gee723 <ben.m.goldschmidt@gmail.com>
2022-02-25 13:22:48 +0100

    function convert 1 CSS object to React CSS object

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cssToReactStyleConverter
 A small project to see if I can convert a CSS stylesheet to react stylesheet.create format
+
+
+## Method
+1.  Take in a string
+2. Remove the spaces
--- index.js
+++ index.js
@@ -1,50 +1,55 @@
 let testCss = `.bg-main {width: 100%; background-image: url("./bg-bluehex.jpg");background-position: top;background-repeat: no-repeat;background-size: cover;}`;
 
-const cssToReactStyleOne = (string) => {
-    let noSpaces = string.replace(/\s/g, '');
-    let insertedDivisions = "";
+const toCamelCase = (string) => {
+    return string.split("-").map((x, i) => i === 0 ? x : x[0].toUpperCase() + x.substring(1)).join("")
+};
 
-    for(let i = 0; i < noSpaces.length; i++){
-        noSpaces[i] === "{" ? insertedDivisions = [(noSpaces.slice(0, i) + " " + noSpaces.slice(i))].join('') : "";
-    }
-    let split = insertedDivisions.split(" ");
+const arrayToObject = (array) => {
     let newObject = {};
-    let secondHalf = split[1].slice(1).slice(0,-2).split(";").join(":").split(":")
-
-    //
-    // .map(x => {
-    //     let y = x.split(":");
-    //     newObject[y[0].trim()] = y[1].trim()
-    //     return newObject;
-    // })
-    
-    for(let i = 0; i < secondHalf.length; i+=2){newObject[secondHalf[i]] = secondHalf[i + 1]
-        newObject[secondHalf[i]] = secondHalf[i + 1];
-        if(i % 2 === 0) {
-           
-        } else {
-            
-        }
+    for (let i = 0; i < array.length; i += 2) {
+        newObject[toCamelCase(array[i])] = array[i + 1];
     }
-    
-    // newObject[split[0].slice(1)] = secondHalf;
-    console.log(secondHalf)
-    // console.log(newObject)
+    return newObject;
 }
 
-cssToReactStyleOne(testCss)
+const cssToReactStyleOne = (string) => {
+    // remove - unecessary spaces
+    let noSpaces = string.replace(/\s/g, '');
+
+    // separate - first half and second half
+    let insertedDivisions = "";
+    for (let i = 0; i < noSpaces.length; i++) {
+        noSpaces[i] === "{" ? insertedDivisions = [(noSpaces.slice(0, i) + " " + noSpaces.slice(i))].join('') : "";
+    }
+
+    // deconstruct - main key and main object
+    let [newKey, newValue] = insertedDivisions.split(" ");
+
+    // create - an array
+    let secondHalf = newValue.slice(1).slice(0, -2).split(";").join(":").split(":");
+
+    // convert - array to object
+    let newObject = arrayToObject(secondHalf);
+
+    // set - new array in to React CSS object format!
+    let reactCss = {};
+    reactCss[newKey] = newObject;
+    return reactCss;
+}
+
+console.log(convertOneCssToReact(testCss))
 
 
 
 // To think odd and even
 const cssToReactStyleMultiple = (string) => {
     let noSpaces = string.replace(/\s/g, '');
+    console.log(string)
     let insertedDivisions = "";
 
-    for(let i = 0; i < noSpaces.length; i++){
+    for (let i = 0; i < noSpaces.length; i++) {
         noSpaces[i] === "{" ? insertedDivisions = [(noSpaces.slice(0, i) + " " + noSpaces.slice(i))].join('') : "";
-        // noSpaces[i] === "}" ? noSpaces = [noSpaces.slice(0, i + 1), " ", noSpaces.slice(i + 1)].join('')  : "";
     }
     let split = insertedDivisions.split(" ")
-    console.log(split)
+    // console.log(split)
 }
